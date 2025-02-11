a07a5d4cd2f2a387d72be0fd71a5985878fab209
Fopefoluwa Ikufisile <fope.ikufisile@gmail.com>
2025-02-11 17:58:34 -0600

    added the ready section (not yet styled properly)

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import Header from "./Components/Header.jsx";
 import TicketSelection from "./Components/TicketSelection.jsx";
 import AttendeeDetails from "./Components/AttendeeDetails.jsx";
+import Ready from "./Components/Ready.jsx";
 
 const App = () => {
   return (
     <>
       <Header />
-      {/* <TicketSelection /> */}
+      <TicketSelection />
       <AttendeeDetails />
+      <Ready/>
     </>
   );
 };
---
+++ src/Components/Ready.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import Ticket from "/public/ticket.svg"
+
+const Ready = () => {
+  return (
+    <form action={""} className="ready">
+      <div className="header">
+        <div className="header-nav">
+          <p className="ticket">Ready</p>
+          <p className="step">Step 3 / 3</p>
+        </div>
+        <div className="progress">
+          <div className="progress-bar-ready"></div>
+        </div>
+      </div>
+      <div className="ready-content">
+        <div className="ready-box">
+          <div className="ready-texts">
+            <p className="ready-ticket">Your Ticket is Booked!</p>
+            <p className="download">
+              You can download or Check your email for a copy
+            </p>
+          </div>
+          <div className="ticket-and-buttons">
+            <div className="ticket-main">
+                <img src={Ticket} alt="" />
+            </div>
+            <div className="nav-buttons">
+              <button className="back">Book another Ticket</button>
+              <button className="get">Download Ticket</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </form>
+  );
+};
+
+export default Ready;
